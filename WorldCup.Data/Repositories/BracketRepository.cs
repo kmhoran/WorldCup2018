9ab7a28c13efe4c13d8f3c26124d1873f20de50d
Kevin <kevin.michael.horan@gmail.com>
2018-04-04 17:02:56 -0700

    update .gitignore

---
+++ WorldCup.Data/Repositories/BracketRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldCup.Data.Contracts;
+
+namespace WorldCup.Data.Repositories
+{
+    internal class BracketRepository: IBracketRepository
+    {
+
+
+    }
+}
